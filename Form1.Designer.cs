4bdaf60d2f747c6c1a4dcc6c5471c3cbaee8a62a
Irfan <irfaanacar@gmail.com>
2023-09-15 22:07:54 +0300

    CSharp How To Use MenuStrip And RichTextBox
    This time, I tried to code how the menustrip feature and richtextbox feature are used in the application. By The Way, Sorry for my English.

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace MenüStripVeRichTextBoxKullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.komediToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aksiyonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maceraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.korkuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.felektenBirGeceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.heartOfStoneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asteriksVeOburiksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testere7ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.tolgshowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zodyakŞövalyeleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avatarSuyunYoluToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bedduaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Black;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.komediToolStripMenuItem,
+            this.aksiyonToolStripMenuItem,
+            this.maceraToolStripMenuItem,
+            this.korkuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(830, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // komediToolStripMenuItem
+            // 
+            this.komediToolStripMenuItem.BackColor = System.Drawing.Color.Black;
+            this.komediToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.felektenBirGeceToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.tolgshowToolStripMenuItem});
+            this.komediToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.komediToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.komediToolStripMenuItem.Name = "komediToolStripMenuItem";
+            this.komediToolStripMenuItem.Size = new System.Drawing.Size(94, 32);
+            this.komediToolStripMenuItem.Text = "Komedi";
+            // 
+            // aksiyonToolStripMenuItem
+            // 
+            this.aksiyonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.heartOfStoneToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.zodyakŞövalyeleriToolStripMenuItem});
+            this.aksiyonToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.aksiyonToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.aksiyonToolStripMenuItem.Name = "aksiyonToolStripMenuItem";
+            this.aksiyonToolStripMenuItem.Size = new System.Drawing.Size(95, 32);
+            this.aksiyonToolStripMenuItem.Text = "Aksiyon";
+            // 
+            // maceraToolStripMenuItem
+            // 
+            this.maceraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asteriksVeOburiksToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.avatarSuyunYoluToolStripMenuItem});
+            this.maceraToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.maceraToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.maceraToolStripMenuItem.Name = "maceraToolStripMenuItem";
+            this.maceraToolStripMenuItem.Size = new System.Drawing.Size(90, 32);
+            this.maceraToolStripMenuItem.Text = "Macera";
+            // 
+            // korkuToolStripMenuItem
+            // 
+            this.korkuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testere7ToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.bedduaToolStripMenuItem});
+            this.korkuToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.korkuToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.korkuToolStripMenuItem.Name = "korkuToolStripMenuItem";
+            this.korkuToolStripMenuItem.Size = new System.Drawing.Size(78, 32);
+            this.korkuToolStripMenuItem.Text = "Korku";
+            // 
+            // felektenBirGeceToolStripMenuItem
+            // 
+            this.felektenBirGeceToolStripMenuItem.Name = "felektenBirGeceToolStripMenuItem";
+            this.felektenBirGeceToolStripMenuItem.Size = new System.Drawing.Size(246, 32);
+            this.felektenBirGeceToolStripMenuItem.Text = "Felekten bir gece";
+            this.felektenBirGeceToolStripMenuItem.Click += new System.EventHandler(this.felektenBirGeceToolStripMenuItem_Click);
+            // 
+            // heartOfStoneToolStripMenuItem
+            // 
+            this.heartOfStoneToolStripMenuItem.Name = "heartOfStoneToolStripMenuItem";
+            this.heartOfStoneToolStripMenuItem.Size = new System.Drawing.Size(263, 32);
+            this.heartOfStoneToolStripMenuItem.Text = "Heart Of Stone";
+            this.heartOfStoneToolStripMenuItem.Click += new System.EventHandler(this.heartOfStoneToolStripMenuItem_Click);
+            // 
+            // asteriksVeOburiksToolStripMenuItem
+            // 
+            this.asteriksVeOburiksToolStripMenuItem.Name = "asteriksVeOburiksToolStripMenuItem";
+            this.asteriksVeOburiksToolStripMenuItem.Size = new System.Drawing.Size(264, 32);
+            this.asteriksVeOburiksToolStripMenuItem.Text = "Asteriks ve Oburiks";
+            this.asteriksVeOburiksToolStripMenuItem.Click += new System.EventHandler(this.asteriksVeOburiksToolStripMenuItem_Click);
+            // 
+            // testere7ToolStripMenuItem
+            // 
+            this.testere7ToolStripMenuItem.Name = "testere7ToolStripMenuItem";
+            this.testere7ToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
+            this.testere7ToolStripMenuItem.Text = "Testere 7";
+            this.testere7ToolStripMenuItem.Click += new System.EventHandler(this.testere7ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(243, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(260, 6);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(261, 6);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(221, 6);
+            // 
+            // tolgshowToolStripMenuItem
+            // 
+            this.tolgshowToolStripMenuItem.Name = "tolgshowToolStripMenuItem";
+            this.tolgshowToolStripMenuItem.Size = new System.Drawing.Size(246, 32);
+            this.tolgshowToolStripMenuItem.Text = "Tolgshow";
+            // 
+            // zodyakŞövalyeleriToolStripMenuItem
+            // 
+            this.zodyakŞövalyeleriToolStripMenuItem.Name = "zodyakŞövalyeleriToolStripMenuItem";
+            this.zodyakŞövalyeleriToolStripMenuItem.Size = new System.Drawing.Size(263, 32);
+            this.zodyakŞövalyeleriToolStripMenuItem.Text = "Zodyak Şövalyeleri";
+            // 
+            // avatarSuyunYoluToolStripMenuItem
+            // 
+            this.avatarSuyunYoluToolStripMenuItem.Name = "avatarSuyunYoluToolStripMenuItem";
+            this.avatarSuyunYoluToolStripMenuItem.Size = new System.Drawing.Size(264, 32);
+            this.avatarSuyunYoluToolStripMenuItem.Text = "Avatar: Suyun Yolu";
+            // 
+            // bedduaToolStripMenuItem
+            // 
+            this.bedduaToolStripMenuItem.Name = "bedduaToolStripMenuItem";
+            this.bedduaToolStripMenuItem.Size = new System.Drawing.Size(224, 32);
+            this.bedduaToolStripMenuItem.Text = "Beddua";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(617, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ACARFLIX";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 148);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(767, 213);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(830, 508);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem komediToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem felektenBirGeceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aksiyonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem heartOfStoneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maceraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asteriksVeOburiksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem korkuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem testere7ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem tolgshowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zodyakŞövalyeleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avatarSuyunYoluToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bedduaToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenüStripVeRichTextBoxKullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void felektenBirGeceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Felekten bir gece";
+            richTextBox1.Text = "Filmde arkadaşlarının düğününden iki gün önce bekârlığa veda partisi vermek için Las Vegas'a giden dört tuhaf arkadaşın, sarhoş oldukları parti gecesinin sabahında odalarında bir kaplan, tavuklar ve dolapta ağlayan altı aylık bir bebek ile uyanmaları üzerine gelişen olağanüstü maceralar aktarılmaktadır.";
+            
+        }
+
+        private void heartOfStoneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Heart Of Stone";
+            richTextBox1.Text = "Heart of Stone, zorlu bir görevle karşı karşıya olan bir ajanın hikayesini konu ediyor. Rachel Stone, kendini barışı korumaya adamış çok uluslu güçlü bir istihbarat teşkilatı olan The Charter'da ajan olarak çalışan bir kadındır. Rachel, zor bir görevle karşı karşıyadır.";
+        }
+
+        private void asteriksVeOburiksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Asteriks Ve Oburiks";
+            richTextBox1.Text = "Asteriks ve Oburiks: Orta Krallık, imparatoriçeyi kurtarmak için zorlu bir maceraya atılan Asteriks, Oburiks ve imparatoriçenin kızı Ra-Na'nın hikayesini konu ediyor. Yıl M.Ö. 50. Kötü prens Deng Tsin Qin, Çin İmparatoriçesi'ni esir almıştır.";
+        }
+
+        private void testere7ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Testere 7";
+            richTextBox1.Text = "Jigsaw'un ölümcül yasası devam ederken Jigsaw'dan kurtulan bir grup insan kendilerine yardımcı olmak için destek ararlar ve ve bu destekçilerin en başında Bobby Dagen adında Jigsaw'dan kurtulmuş biri vardır. Bobby'nin gizli sırlarından yeni bir terör dalgası ortaya çıkacaktır.";
+        }
+    }
+}
